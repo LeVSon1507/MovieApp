dcd5137a34c68128c271197dd126eb3ea7a3345f
Le V Son <sonlvs1507@gmail.com>
2023-04-16 18:30:07 +0700

    build product

---
+++ build/precache-manifest.7957999f010ce00a9cb7f0887eaeb86d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a606213b06bbb77dd828e8fd902a9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fbbf63c5ec6e70ebbc5",
+    "url": "/static/css/2.68f0c4ae.chunk.css"
+  },
+  {
+    "revision": "66200611b485a583ec70",
+    "url": "/static/css/main.2a36c250.chunk.css"
+  },
+  {
+    "revision": "7fbbf63c5ec6e70ebbc5",
+    "url": "/static/js/2.dbdb62fb.chunk.js"
+  },
+  {
+    "revision": "7b8baa934adf4c6b621e7292b4c767e2",
+    "url": "/static/js/2.dbdb62fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66200611b485a583ec70",
+    "url": "/static/js/main.3aad043c.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.3aad043c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260705052eb5a5c8bd41",
+    "url": "/static/js/runtime-main.d3cf5db1.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
